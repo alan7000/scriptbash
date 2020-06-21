b3952aba176d23eaec612b92d7fa7c349cd64e85
Flambard alan <7330585+alan7000@users.noreply.github.com>
2020-06-21 23:55:10 +0200

    Update Menu_script.sh

--- Menu_script.sh
+++ Menu_script.sh
@@ -2,9 +2,10 @@
 echo "Voici un menu de choix pour choisir le script que vous souhaiter."
 echo ""
 echo "Selectionner \"vaapi\" pour utiliser le script d'activation de l'acceleration matériel."
-echo "Selectionner \"grub\" pour utiliser le script de mise en place du bon clavier pour grub."
+echo "Selectionner \"grub-fr\" pour utiliser le script de mise en place du bon clavier pour grub."
+echo "Selectionner \"grub-show\" pour utiliser le script pour rendre visible grub."
 echo ""
-read -p "Faite votre choix vaapi ou grub : " option
+read -p "Faite votre choix vaapi, grub-fr ou grub-show : " option
 echo ""
 
 case $option in
@@ -12,8 +13,12 @@ case $option in
     ./install-vaapi.sh
     ;;
 
-    grub)
+    grub-fr)
     ./keyboard-grub.sh
+    ;;
+
+    grub-show)
+    ./show-grub.sh
     ;;
 
     *)
