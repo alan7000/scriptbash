fb072e2180bb4bf1787ba6e96702e6d096684867
Flambard alan <7330585+alan7000@users.noreply.github.com>
2020-06-22 00:02:11 +0200

    Create show-grub.sh

---
+++ show-grub.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+link=/etc/default/grub.d/90_custom.cfg
+echo "Création du fichier custom."
+sudo touch $link
+
+echo ""
+echo "Ajout des paramètre de grub dans le fichier custom."
+sudo sh -c "echo '# Custom config' >> $link
+sudo sh -c "echo 'GRUB_TIMEOUT="5"' >> $link
+sudo sh -c "echo 'GRUB_TIMEOUT_STYLE="menu"' >> $link
+
+echo ""
+echo "Mise a jour de GRUB."
+sudo update-grub
+
+echo ""
+echo "Grub est a présent visible."
+read -a a
